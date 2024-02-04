0003abb1a45dc0859242efa8a59d5835f64e81db
cooscoos <33428786+cooscoos@users.noreply.github.com>
2024-02-04 06:29:41 +0000

    nice

--- src/main.rs
+++ src/main.rs
@@ -20,49 +20,61 @@ async fn main() {
     println!("{}", HELP);
 
     let pause = Arc::new(AtomicBool::new(false));
-    tokio::spawn(elapsed_time(pause.clone()));
-    let keypress_handle = tokio::spawn(keypress_listen(pause));
+    let quit = Arc::new(AtomicBool::new(false));
+    let elapsed_time_handle = tokio::spawn(elapsed_time(pause.clone(), quit.clone()));
+    let keypress_handle = tokio::spawn(keypress_listen(pause, quit));
 
     tokio::select! {
-    _ = keypress_handle => {
-        println!("\nExited.");
-    }
+        _ = keypress_handle => {
+            let elapsed = elapsed_time_handle.await.unwrap();
+            println!("\r\nExited. Total elapsed time: {:?}", elapsed);
+        }
     }
 }
 
 /// Checks for keypresses every 100 ms and acts accordingly.
-async fn keypress_listen(pause: Arc<AtomicBool>) {
+async fn keypress_listen(pause: Arc<AtomicBool>, quit: Arc<AtomicBool>) {
     // raw mode allows keypresses to be read without needing to press enter.
     terminal::enable_raw_mode().unwrap();
 
     loop {
-        if event::poll(std::time::Duration::from_millis(100)).unwrap() {
-            if let Event::Key(key) = event::read().unwrap() {
-                match key.code {
-                    KeyCode::Char('q') => break,
+        if event::poll(Duration::from_millis(100)).unwrap() {
+            if let Event::Key(event) = event::read().unwrap() {
+                match event.code {
+                    KeyCode::Char('q') => {
+                        // Set the quit AtomicBool to true when 'q' is pressed
+                        quit.store(true, Ordering::Relaxed);
+                        terminal::disable_raw_mode().unwrap();
+                        break;
+                    }
                     KeyCode::Char('p') => {
                         let current = pause.load(Ordering::Relaxed);
                         pause.store(!current, Ordering::Relaxed);
                     }
-                    _ => (),
+                    _ => {}
                 }
             }
         }
     }
-
-    terminal::disable_raw_mode().unwrap();
 }
 
 /// Prints elapsed time every second and handles pause toggles.
-async fn elapsed_time(pause: Arc<AtomicBool>) {
+/// Returns the elapsed time when the function ends.
+async fn elapsed_time(pause: Arc<AtomicBool>, quit: Arc<AtomicBool>) -> chrono::Duration {
     let mut start_time = Local::now();
     let mut pause_start_time: Option<chrono::DateTime<Local>> = None;
+    let mut elapsed = chrono::Duration::zero();
 
     loop {
         tokio::time::sleep(Duration::from_secs(1)).await;
-        let is_paused = pause.load(Ordering::Relaxed);
 
-        if is_paused {
+        // Stop and return elapsed time if 'q' button pressed
+        if quit.load(Ordering::Relaxed) {
+            break;
+        }
+
+        // Pause or resume if 'p' button pressed
+        if pause.load(Ordering::Relaxed) {
             if pause_start_time.is_none() {
                 pause_start_time = Some(Local::now());
             }
@@ -71,14 +83,16 @@ async fn elapsed_time(pause: Arc<AtomicBool>) {
                 start_time = start_time + (Local::now() - pause_start);
                 pause_start_time = None;
             }
-            let elapsed = Local::now() - start_time;
+            elapsed = Local::now() - start_time;
             print!(
                 "\rElapsed time: {:02}:{:02}:{:02}",
                 elapsed.num_hours(),
-                elapsed.num_minutes(),
-                elapsed.num_seconds()
+                elapsed.num_minutes() % 60,
+                elapsed.num_seconds() % 60
             );
             io::stdout().flush().unwrap();
         }
     }
+
+    elapsed
 }
